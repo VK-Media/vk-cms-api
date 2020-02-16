7ff86158f654ba3d629d563e1258efb5c8ec0cc4
Robin Louis van Komen <robin@vkmedia.dk>
2020-02-16 15:19:21 +0100

    add filters and limits to uploads

--- src/core/media/utils/media.utils.ts
+++ src/core/media/utils/media.utils.ts
@@ -17,6 +17,26 @@ const storage = multer.diskStorage({
 	}
 })
 
+const limits = {
+	fileSize: 2 * 1024 * 1024
+}
+
+const fileFilter = (req, file, cb) => {
+	const fileExtension = file.originalname.split('.').pop()
+
+	if(fileExtension in allowedFileTypes){
+		if(allowedFileTypes[fileExtension] === file.mimetype){
+			cb(null, true)
+		} else {
+			cb(new Error('Mimetype and file extensions does not match'))
+		}
+	} else {
+		cb(new Error('The file type is not allowed'))
+	}
+}
+
+export const upload = multer({ storage, limits, fileFilter })
+
 export const getUniqueFilename = (destination: string, filename: string): string => {
 	let uniqueFilename = filename
 	let counter = 0
@@ -36,8 +56,6 @@ export const getSafeFilename = (originalFilename: string): string => {
 	return slugify(originalFilename, { remove: /"<>#%{}\|\\\^~\[]`;\?:@=&/g }).toLowerCase()
 }
 
-export const upload = multer({ storage })
-
 export const getFolderFromRequest = (req: Request): IFolder => {
 	const path = ['./media']
 
@@ -55,4 +73,25 @@ export const getFolderFromRequest = (req: Request): IFolder => {
 		path: fullPath,
 		exists: fs.existsSync(fullPath)
 	}
+}
+
+export const allowedFileTypes = {
+	csv: 'text/csv',
+	doc: 'application/msword',
+	docx: 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+	gif: 'image/gif',
+	jpeg: 'image/jpeg',
+	jpg: 'image/jpeg',
+	mp3: 'audio/mpeg',
+	mpeg: 'video/mpeg',
+	png: 'image/png',
+	pdf: 'application/pdf',
+	ppt: 'application/vnd.ms-powerpoint',
+	pptx: 'application/vnd.openxmlformats-officedocument.presentationml.presentation',
+	svg: 'image/svg+xml',
+	txt: 'text/plain',
+	webm: 'video/webm',
+	webp: 'image/webp',
+	xls: 'application/vnd.ms-excel',
+	xlsx: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
 }
